5da76de58b0b8ed475898fb1d35a6da2857b9454
Matheus <matheus.luiz@grupointegrado.br>
2026-05-24 12:44:12 -0300

    site pronto

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,16 @@
 <div class="container">
         <div class="banner">
         <img src="imgs/home.png" alt="home-img">
+        <a href="index.php?pagina=cardapio" title="verCardapio" class="text-feijoada">
+                        Clique aqui para ver o cardápio
+                </a>
         </div>
-        <h2>Bem vindos a marmitaria Sabôr do Céu</h2>
+        
         <div class="banner">
                 <a href="index.php?pagina=cardapio" title="Feijoada">
                         <img src="imgs/feijoada.jpg" alt="Feijoada">
+                
                 </a>
+              
         </div>
 </div>
